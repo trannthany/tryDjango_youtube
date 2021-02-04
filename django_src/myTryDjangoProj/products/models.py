e3e8c891b646afafec0fba80dd6b8b16228345ac
trannthany <trannthany@gmail.com>
2021-02-05 11:37:40 +1300

    make changes to product model hehe

--- django_src/myTryDjangoProj/products/models.py
+++ django_src/myTryDjangoProj/products/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Product():
-    title = models.
+class Product(models.Model): #I inheritted some functions and attributes from models.Model
+    title       = models.TextField()
+    description = models.TextField()
+    price       = models.TextField()
